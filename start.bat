29e6621122d4906ac0f4067cf6cbc4002aa14e60
HXYMCH <102237225+HXYMCH@users.noreply.github.com>
2022-04-09 21:50:10 +0800

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,4 +1,17 @@
+:: !/bin/bash
+
+goto information
+#################################################
+# Author : HXYMCH
+# Date : 2022-4-9
+# github : https://github.com/HXYMCH/py-terminal
+#################################################
+:information
+
 :: run py-terminal
 dir
+cd app
+dir
+cd ..
 python pyTerminal.py
 pause
